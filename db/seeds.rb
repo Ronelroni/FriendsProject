11a6198312d3a04f57197f5701ee768c80e77c88
Ronelroni <ronelroni10@gmail.com>
2021-12-20 19:47:10 +0100

    correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 5.times do |n|
-    name = Faker::Games::Pokemon.name
-    email = Faker::Internet.email
+    name = "name#{n}"
+    email = "email#{n}add@xel.com"
+    puts email
     password = "password"
     user = User.create!(name: name, email: email, password: password)
     5.times do |n|
-        image = Faker::LoremPixel.image
-        content = Faker::Lorem.word
+        image = "image#{n}"
+        content = "word#{n}"
         publication = Publication.create!(user_id: user.id, image: image, content: content)
         5.times do |n|
-            content = Faker::Lorem.word
+            content = "word#{n}"
             Comment.create!(user_id: user.id, publication_id: publication.id, content: content)
             
         end
@@ -23,3 +24,7 @@
     end
     
 end
+5.times do |n|
+    Group.create!(name:"name#{n}")
+end  
+
--- spec/system/user_signs_in_spec.rb
+++ spec/system/user_signs_in_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature 'User signs in', type: :system do
     fill_in 'user[password]', with: user.password
     click_button 'Connecter'
 
-    expect(page).to have_text 'NOUVELLE PUBLICATION'
+    expect(page).to have_text 'Nouvelle publication'
   end
 
   scenario 'with invalid credentials' do
